fd47131116255a3dce65e38295edbc6895212cd9
rlorenzini <richard@Richards-MBP.localdomain>
2019-02-12 12:59:43 -0600

    first fullname edition

---
+++ fullname.py
@@ -0,0 +1,7 @@
+#My name is first, last
+#use inputs
+
+first_name = input('Enter First Name: ')
+last_name = input('Enter Last Name: ')
+
+print("Hello, my name is " + first_name + " " + last_name + ".")
