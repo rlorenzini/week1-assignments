db22026b7d1e35b44798b9b8bb8796a4e986ecf0
rlorenzini <richard@Richards-MBP.localdomain>
2019-02-12 12:29:53 -0600

    first edition of two number calculator

---
+++ two_numbers.py
@@ -0,0 +1,13 @@
+#input two numbers from user
+#have both numbers added
+#print results for user to see
+
+no1 = input('Number one: ')
+no2 = input('Number two: ')
+
+def add(a,b):
+    result = float(a) + float(b)
+    return result
+
+result = add(no1,no2)
+print('And the total is... ' + str(result) + '.')
