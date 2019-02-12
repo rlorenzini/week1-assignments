48652a6c8723e6b8f937ec4ce320108dccd2ff08
rlorenzini <richard@Richards-MBP.localdomain>
2019-02-12 12:14:34 -0600

    tip calculator first edition

---
+++ tip_calculator.py
@@ -0,0 +1,16 @@
+#enter total
+#enter tip
+#create function to calculate tip and print() for user
+
+
+total_amount = input('Enter Total Amount: ')
+tip_amount = input('Enter Tip %: ')
+
+def tip_percent(a,b):
+    #return total_amount / tip_amount = tip_total
+    tip_total = float(a) * float(b) / 100
+    return tip_total
+
+tip_total = tip_percent(total_amount,tip_amount)
+#print("Tip total would be " + tip_percent) error: cannot concatenate str to str; error with float?
+print("Your tip would be " + str(tip_total) + "%.")
